21c7d1e92e49822473c394f9affe178ea989d0a2
Steven McAndrew <steven.mcandrew@outlook.com>
2018-10-26 14:22:41 +0100

    completed CRUD on tag class

--- models/tag.rb
+++ models/tag.rb
@@ -17,3 +17,24 @@ def save()
   results = SqlRunner.run(sql, values)
   @id = results.first()['id'].to_i
 end
+
+def self.all()
+  sql = "SELECT * FROM tags"
+  results = SqlRunner.run(sql)
+  return results.map{|tag| Tag.new(tag)}
+end
+
+def self.find(id)
+  sql = "SELECT * FROM tags
+  WHERE id = $1"
+  values = [id]
+  results = SqlRunner.run(sql, values)
+  return Tag.new(results.first)
+end
+
+def self.delete_all
+  sql = "DELETE FROM tags"
+  SqlRunner.run(sql)
+end
+
+end
